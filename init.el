e5fdc0385964e75f8c8b7e3bdff85342b0f1dd71
YAMADA Koji <kou64yama@gmail.com>
2018-11-20 23:26:58 +0900

    :lipstick: Disable transparency

--- init.el
+++ init.el
@@ -87,7 +87,6 @@
 ;; -----------------------------------------------------------------------------
 
 (when window-system
-  (add-to-list 'default-frame-alist '(alpha . .85))
   (add-to-list 'default-frame-alist '(width . 100))
   (add-to-list 'default-frame-alist '(height . 30)))
 
