4eedcdb4615f62bbc70446ed9a0e9d89f7940194
YAMADA Koji <kou64yama@gmail.com>
2019-02-18 00:44:30 +0900

    :sparkles: Update

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # ~/.emacs.d
 
-My emacs configurations.
+My Emacs configurations.
 
 ## Requirements
 
-* [Emacs](https://www.gnu.org/software/emacs/) 25+
+- [Emacs](https://www.gnu.org/software/emacs/) 25+
 
 ## Installation
 
@@ -14,85 +14,46 @@ Checkout this repository at `~/.emacs.d`:
 $ git clone https://github.com/kou64yama/dot.emacs.d.git ~/.emacs.d
 ```
 
-## Key bindings
-
-* global-map
-  * `C-x g`: Invoke `magit-status`.
-  * `C-x M-g`: Invoke `magit-dispatch-popup`.
-  * `C-=`: Invoke `er/expand-region`.
-  * `C-S-c C-S-c`: Invoke `mc/edit-lines`.
-  * `C-<`: Invoke `mc/mark-previous-like-this`.
-  * `C->`: Invoke `mc/mark-next-like-this`.
-  * `C-c C-<`: Invoke `mc/mark-all-like-this`.
-  * `C-c t`: Invoke `tabbar-ruler-move`.
-  * `C-x C-j`: Invoke `ssk-mode`.
-  * `C-c C-r` or `<f6>`: Invoke `ivy-resume`.
-  * `M-x`: Invoke `counsel-M-x`.
-  * `C-x C-f`: Invoke `counsel-find-file`.
-  * `<f1> f`: Invoke `counsel-describe-function`.
-  * `<f1> v`: Invoke `counsel-describe-variable`.
-  * `<f1> l`: Invoke `counsel-find-library`.
-  * `<f2> i`: Invoke `counsel-info-lookup-symbol`.
-  * `<f2> u`: Invoke `counsel-unicode-char`.
-  * `C-c g`: Invoke `counsel-git`.
-  * `C-c j`: Invoke `counsel-git-grep`.
-  * `C-c k`: Invoke `counsel-ag`.
-  * `C-x l`: Invoke `counsel-locate`.
-  * `C-S-o`: Invoke `counsel-rhythmbox`.
-  * `C-s`: Invoke `swiper`.
-  * `<M-up>`: Invoke `drag-stuff-up`
-  * `<M-down>`: Invoke `drag-stuff-down`
-  * `<M-right>`: Invoke `drag-stuff-right`
-  * `<M-left>`: Invoke `drag-stuff-left`
-* read-expression-map
-  * `C-r`: Invoke `counsel-expression-history`.
-
 ## Packages
 
-* [adoc-mode](https://github.com/sensorflo/adoc-mode/wiki)
-* [company-flow](https://github.com/aaronjensen/company-flow)
-* [company-go](https://github.com/nsf/gocode/tree/master/emacs-company)
-* [company](https://company-mode.github.io)
-* [counsel-projectile](https://github.com/ericdanan/counsel-projectile)
-* [counsel](https://github.com/abo-abo/swiper#counsel)
-* [ddskk](http://openlab.jp/skk/)
-* [drag-stuff](https://github.com/rejeep/drag-stuff.el)
-* [editorconfig](https://github.com/editorconfig/editorconfig-emacs)
-* [exec-path-from-shell](https://github.com/purcell/exec-path-from-shell)
-* [expand-region](https://github.com/magnars/expand-region.el)
-* [flycheck-flow](https://github.com/lbolla/emacs-flycheck-flow)
-* [flycheck](http://www.flycheck.org/en/latest/)
-* [font-utils](https://github.com/rolandwalker/font-utils)
-* [git-gutter-fringe+](https://github.com/nonsequitur/git-gutter-fringe-plus)
-* [gitattributes-mode](https://github.com/magit/git-modes/blob/master/gitattributes-mode.el)
-* [gitconfig-mode](https://github.com/magit/git-modes/blob/master/gitconfig-mode.el)
-* [gitignore-mode](https://github.com/magit/git-modes/blob/master/gitignore-mode.el)
-* [go-mode](https://github.com/dominikh/go-mode.el)
-* [google-translate](https://github.com/atykhonov/google-translate)
-* [groovy-mode](https://github.com/Groovy-Emacs-Modes/groovy-emacs-modes)
-* [ivy](https://github.com/abo-abo/swiper#ivy)
-* [js2-mode](https://github.com/mooz/js2-mode)
-* [json-mode](https://github.com/joshwnj/json-mode)
-* [keyfreq](https://github.com/dacap/keyfreq)
-* [magit](https://magit.vc)
-* [magithub](https://github.com/vermiculus/magithub)
-* [markdown-mode](http://jblevins.org/projects/markdown-mode/)
-* [mode-icons](http://projects.ryuslash.org/mode-icons/)
-* [multiple-cursors](https://github.com/magnars/multiple-cursors.el)
-* [nord-theme](https://emacsthemes.com/themes/nord-theme.html)
-* [projectile](https://projectile.readthedocs.io/en/latest/)
-* [rainbow-delimiters](https://github.com/Fanael/rainbow-delimiters)
-* [smart-mode-line-powerline-theme](https://github.com/Malabarba/smart-mode-line/)
-* [smartparens](https://github.com/Fuco1/smartparens)
-* [swiper](https://github.com/abo-abo/swiper#swiper)
-* [tide](https://github.com/ananthakumaran/tide)
-* [typescript-mode](https://github.com/ananthakumaran/typescript.el)
-* [undo-tree](https://www.emacswiki.org/emacs/UndoTree)
-* [undohist](https://github.com/m2ym/undohist-el)
-* [use-package](https://github.com/jwiegley/use-package)
-* [web-mode](http://web-mode.org)
-* [yascroll](https://github.com/m2ym/yascroll-el)
-* [yasnippet](https://joaotavora.github.io/yasnippet/)
+- [add-node-modules-path](https://github.com/codesuki/add-node-modules-path)
+- [benchmark-init](https://github.com/dholm/benchmark-init-el)
+- [company](https://company-mode.github.io)
+- [counsel](https://github.com/abo-abo/swiper#counsel)
+- [dashboard](https://github.com/rakanalh/emacs-dashboard)
+- [ddskk](http://openlab.jp/skk/)
+- [docker](https://github.com/Silex/docker.el)
+- [dockerfile-mode](https://github.com/spotify/dockerfile-mode)
+- [drag-stuff](https://github.com/rejeep/drag-stuff.el)
+- [editorconfig](https://github.com/editorconfig/editorconfig-emacs)
+- [emojify](https://github.com/iqbalansari/emacs-emojify)
+- [exec-path-from-shell](https://github.com/purcell/exec-path-from-shell)
+- [expand-region](https://github.com/magnars/expand-region.el)
+- [flycheck](http://www.flycheck.org/en/lattest/)
+- [gitattributes-mode](https://github.com/magit/git-modes/blob/master/gitattributes-mode.el)
+- [gitconfig-mode](https://github.com/magit/git-modes/blob/master/gitconfig-mode.el)
+- [gitignore-mode](https://github.com/magit/git-modes/blob/master/gitignore-mode.el)
+- [ivy](https://github.com/abo-abo/swiper#ivy)
+- [js2-mode](https://github.com/mooz/js2-mode)
+- [json-mode](https://github.com/joshwnj/json-mode)
+- [keyfreq](https://github.com/dacap/keyfreq)
+- [magit](https://magit.vc)
+- [markdown-mode](https://jblevins.org/projects/markdown-mode)
+- [mode-icons](http://projects.ryuslash.org/mode-icons/)
+- [multiple-cursors](https://github.com/magnars/multiple-cursors.el)
+- [nord-theme](https://emacsthemes.com/themes/nord-theme.html)
+- [projectile](https://projectile.readthedocs.io/en/latest/)
+- [rainbow-delimiters](https://github.com/Fanael/rainbow-delimiters)
+- [smart-mode-line-powerline-theme](https://github.com/Malabarba/smart-mode-line)
+- [smartparens](https://github.com/Fuco1/smartparens)
+- [swiper](https://github.com/abo-abo/swiper#swiper)
+- [tide](https://github.com/ananthakumaran/tide)
+- [typescript-mode](https://github.com/emacs-typescript/typescript.el)
+- [undo-tree](https://www.emacswiki.org/emacs/UndoTree)
+- [undohist](https://github.com/m2ym/undohist-el)
+- [use-package](https://github.com/jwiegley/use-package)
+- [web-mode](http://web-mode.org)
+- [yaml-mode](https://github.com/yoshiki/yaml-mode)
 
 ## License
 
--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;; init.el -- Emacs configurations
+;;; init.el -- My Emacs Configuration
 ;;; Commentary:
 
 ;; Copyright (c) 2016-present YAMADA Koji.
@@ -8,168 +8,161 @@
 
 ;;; Code:
 
-;; Set `user-emacs-directory` if Emacs is launched with `emacs -q -l init.el`.
 (when load-file-name
   (setq user-emacs-directory (file-name-directory load-file-name)))
 
-;; Load `custom.el`.
 (when (file-exists-p (setq custom-file (locate-user-emacs-file "custom.el")))
   (load custom-file))
 
-;; Add "~/.emacs.d/site-lisp" and "~/.emacs.d/lisp" to `load-path`.
+;;
+;; Setup load-path
+;; -----------------------------------------------------------------------------
+
 (add-to-list 'load-path (locate-user-emacs-file "site-lisp"))
 (add-to-list 'load-path (locate-user-emacs-file "lisp"))
 
-(require 'eplug)
-
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
-
-(eplug 'adoc-mode)
-(eplug 'company)
-(eplug 'company-flow)
-(eplug 'company-go)
-(eplug 'counsel)
-(eplug 'counsel-projectile)
-(eplug 'ddskk)
-(eplug 'drag-stuff)
-(eplug 'editorconfig)
-(eplug 'emojify)
-(eplug 'exec-path-from-shell)
-(eplug 'expand-region)
-(eplug 'flycheck)
-(eplug 'flycheck-flow)
-(eplug 'font-utils)
-(eplug 'git-gutter-fringe+)
-(eplug 'gitattributes-mode)
-(eplug 'gitconfig-mode)
-(eplug 'gitignore-mode)
-(eplug 'go-mode)
-(eplug 'google-translate)
-(eplug 'groovy-mode)
-(eplug 'ivy)
-(eplug 'js2-mode)
-(eplug 'json-mode)
-(eplug 'keyfreq)
-(eplug 'magit)
-(eplug 'magithub)
-(eplug 'markdown-mode)
-(eplug 'mode-icons)
-(eplug 'multiple-cursors)
-(eplug 'nord-theme)
-(eplug 'projectile)
-(eplug 'rainbow-delimiters)
-(eplug 'smart-mode-line-powerline-theme)
-(eplug 'smartparens)
-(eplug 'swiper)
-(eplug 'tide)
-(eplug 'typescript-mode)
-(eplug 'undo-tree)
-(eplug 'undohist)
-(eplug 'use-package)
-(eplug 'web-mode)
-(eplug 'yascroll)
-(eplug 'yasnippet-snippets)
-
-(and (eplug-check)
-     (y-or-n-p (format "Install %s? "
-                       (mapconcat 'symbol-name (eplug-check) ",")))
-     (eplug-install))
-
-(setq backup-inhibited t
-      column-number-mode t
-      inhibit-startup-message -1
-      visible-bell t)
-(setq-default indent-tabs-mode nil)
-
-(when window-system
-  (tool-bar-mode -1)
-  (scroll-bar-mode -1))
-(unless window-system
-  (menu-bar-mode -1))
-
 ;;
-;; Window
+;; Setup package
 ;; -----------------------------------------------------------------------------
 
-(when window-system
-  (add-to-list 'default-frame-alist '(width . 100))
-  (add-to-list 'default-frame-alist '(height . 30)))
+(require 'package)
+
+(defcustom package-contents-last-refreshed nil
+  "Last refreshed time."
+  :group 'package)
+
+(add-to-list 'package-archives
+	     '("melpha" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(let ((now (float-time)))
+  (when (or (not package-contents-last-refreshed)
+	    (> (- now package-contents-last-refreshed) (* 7 24 60 60)))
+    (package-refresh-contents)
+    (customize-save-variable 'package-contents-last-refreshed (float-time))))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
 ;;
 ;; Benchmark
 ;; -----------------------------------------------------------------------------
 
-;; keyfreq
-;; https://github.com/dacap/keyfreq
+(use-package benchmark-init
+  ;; https://github.com/dholm/benchmark-init-el
+  :ensure t
+  :hook ((after-init . benchmark-init/deactivate)))
+
+;;
+;; Environment variables
+;; -----------------------------------------------------------------------------
+
+(use-package exec-path-from-shell
+  ;; https://github.com/purcell/exec-path-from-shell
+  :ensure t
+  :if (member system-type '(gnu/linux ns))
+  :init (exec-path-from-shell-initialize))
+
+;;
+;; Statistics
+;; -----------------------------------------------------------------------------
+
 (use-package keyfreq
+  ;; https://github.com/dacap/keyfreq
+  :ensure t
   :init
   (keyfreq-mode)
   (keyfreq-autosave-mode))
 
 ;;
-;; Environment Variables
+;; Input Method
 ;; -----------------------------------------------------------------------------
 
-;; exec-path-from-shell
-;; https://github.com/purcell/exec-path-from-shell
-(use-package exec-path-from-shell
-  :if (memq window-system '(mac ns x))
-  :init (exec-path-from-shell-initialize))
+(use-package ddskk
+  ;; http://openlab.jp/skk/
+  :ensure t
+  :defer t
+  :bind (("C-x C-j" . skk-mode))
+  :init (setq default-input-method "japanese-skk"))
 
 ;;
-;; Editor
+;; Dashboard
 ;; -----------------------------------------------------------------------------
 
-;; EditorConfig
-;; https://github.com/editorconfig/editorconfig-emacs
-(use-package editorconfig
-  :init (editorconfig-mode))
+(use-package dashboard
+  ;; https://github.com/rakanalh/emacs-dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook))
 
-;; expand-region.el
-;; https://github.com/magnars/expand-region.el
-(use-package expand-region
-  :bind (("C-=" . er/expand-region)))
+;;
+;; Appearance
+;; -----------------------------------------------------------------------------
 
-;; multiple-cursors.el
-;; https://github.com/magnars/multiple-cursors.el
-(use-package multiple-cursors
-  :bind (("C-S-c C-S-c" . mc/edit-lines)
-         ("C-<" . mc/mark-previous-like-this)
-         ("C->" . mc/mark-next-like-this)
-         ("C-c C-<" . mc/mark-all-like-this)))
+(use-package nord-theme
+  ;; https://emacsthemes.com/themes/nord-theme.html
+  :ensure t
+  :init (load-theme 'nord t))
 
-;; Drag Stuff
-;; https://github.com/rejeep/drag-stuff.el
-(use-package drag-stuff
-  :init (drag-stuff-global-mode)
-  :config (drag-stuff-define-keys))
+(use-package smart-mode-line-powerline-theme
+  ;; https://github.com/Malabarba/smart-mode-line
+  :ensure t
+  :init
+  (setq sml/theme 'powerline)
+  (sml/setup))
 
-;; Smartparens
-;; https://github.com/Fuco1/smartparens
-(use-package smartparens-config
-  :init (smartparens-global-mode))
+(use-package mode-icons
+  ;; http://projects.ryuslash.org/mode-icons/
+  :ensure t
+  :init (mode-icons-mode))
 
-;; rainbow-delimiters
-;; https://github.com/Fanael/rainbow-delimiters
-(use-package rainbow-delimiters
-  :init (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
+(use-package emojify
+  ;; https://github.com/iqbalansari/emacs-emojify
+  :ensure t
+  :if window-system
+  :init (global-emojify-mode))
+
+;;
+;; Font
+;; ----------------------------------------------------------------------------------------
+
+(use-package font
+  ;; 567890123456789012345678901234567890123456789012345678901234567890123456789
+  ;; Grumpy wizards make toxic brew for the evil Queen and Jack.
+  ;;
+  ;; あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、
+  ;; うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。
+  ;; 567890123456789012345678901234567890123456789012345678901234567890123456789
+  :if window-system
+  :bind (("C-+" . font/increase-height)
+         ("C--" . font/decrease-height)
+         ("C-0" . font/reset-height))
+  :init
+  (set-face-attribute 'default nil :family "Ricty Diminished")
+  (set-fontset-font t 'japanese-jisx0208 (font-spec :family "Ricty Diminished"))
+  (setq font/default-height 130)
+  (font/reset-height))
 
 ;;
 ;; Minibuffer
 ;; -----------------------------------------------------------------------------
 
-;; Ivy
-;; https://github.com/abo-abo/swiper#ivy
 (use-package ivy
+  ;; https://github.com/abo-abo/swiper#ivy
+  :ensure t
+  :defer t
+  :delight
   :bind (("C-c c-r" . ivy-resume)
          ("<f6>" . ivy-resume))
-  :init (ivy-mode))
+  :init (ivy-mode)
+  :config
+  (setq ivy-re-builders-alist
+        '((t . ivy--regex-fuzzy))))
 
-;; Counsel
-;; https://github.com/abo-abo/swiper#counsel
 (use-package counsel
+  ;; https://github.com/abo-abo/swiper#counsel
+  :ensure t
+  :defer t
   :bind (("M-x" . counsel-M-x)
          ("C-x C-f" . counsel-find-file)
          ("<f1> f" . counsel-describe-function)
@@ -185,235 +178,169 @@
          :map read-expression-map
          ("C-r" . counsel-expression-history)))
 
-;; Swiper
-;; https://github.com/abo-abo/swiper#swiper
 (use-package swiper
+  ;; https://github.com/abo-abo/swiper#swiper
+  :ensure t
+  :defer t
   :bind (("C-s" . swiper)))
 
 ;;
-;; Appearance
+;; Editor
 ;; -----------------------------------------------------------------------------
 
-;; Material Theme
-;; https://emacsthemes.com/themes/nord-theme.html
-(use-package nord-theme
-  :init (load-theme 'nord t))
+(use-package editorconfig
+  ;; https://github.com/editorconfig/editorconfig-emacs
+  :ensure t
+  :delight
+  :init (editorconfig-mode))
 
-;; Smart-mode-line
-;; https://github.com/Malabarba/smart-mode-line/
-(use-package smart-mode-line
+(use-package expand-region
+  ;; https://github.com/magnars/expand-region.el
+  :ensure t
+  :bind (("C-=" . er/expand-region)))
+
+(use-package multiple-cursors
+  ;; https://github.com/magnars/multiple-cursors.el
+  :ensure t
+  :bind (("C-S-c C-S-c" . mc/edit-lines)
+         ("C-<" . mc/mark-previous-like-this)
+         ("C->" . mc/mark-next-like-this)
+         ("C-c C-<" . mc/mark-all-like-this)))
+
+(use-package drag-stuff
+  ;; https://github.com/rejeep/drag-stuff.el
+  :ensure t
+  :delight
+  :init (drag-stuff-global-mode)
+  :config (drag-stuff-define-keys))
+
+(use-package smartparens
+  ;; https://github.com/Fuco1/smartparens
+  :ensure t
+  :delight
   :init
-  (setq sml/theme 'powerline)
-  (sml/setup))
+  (require 'smartparens-config)
+  (smartparens-global-mode))
 
-;; yascroll.el
-;; https://github.com/m2ym/yascroll-el
-(use-package yascroll
-  :if window-system
-  :init (global-yascroll-bar-mode))
-
-;; font-utils
-;; https://github.com/rolandwalker/font-utils
-(use-package font-utils
-  :if window-system
-  :init
-  (let ((family (font-utils-first-existing-font
-                 '("Share Tech Mono"
-                   "Menlo"
-                   "Consolas")))
-        (ja-family (font-utils-first-existing-font
-                    '("Ricty Diminished"
-                      "Yu Gothic"
-                      "IPA Gothic"
-                      "Meiryo"))))
-    (when family (set-face-attribute 'default nil :family family :height (* 15 9)))
-    (when ja-family (set-fontset-font (frame-parameter nil 'font)
-                                      'japanese-jisx0208
-                                      (font-spec :family ja-family)))))
-
-;; Mode icons
-;; http://projects.ryuslash.org/mode-icons/
-(use-package mode-icons
-  :init (mode-icons-mode))
-
-;; Emojify
-;; https://github.com/iqbalansari/emacs-emojify
-(use-package emojify
-  :init (global-emojify-mode))
+(use-package rainbow-delimiters
+  ;; https://github.com/Fanael/rainbow-delimiters
+  :ensure t
+  :defer t
+  :disabled
+  :hook prog-mode)
 
 ;;
 ;; History
 ;; -----------------------------------------------------------------------------
 
-;; Undo Tree
-;; https://www.emacswiki.org/emacs/UndoTree
 (use-package undo-tree
+  ;; https://www.emacswiki.org/emacs/UndoTree
+  :ensure t
+  :delight
   :init (global-undo-tree-mode))
 
-;; undohist
-;; https://github.com/m2ym/undohist-el
 (use-package undohist
+  ;; https://github.com/m2ym/undohist-el
+  :ensure t
+  :defer t
   :commands (undohist-initialize)
   :init (undohist-initialize)
-  :config
-  (setq undohist-ignored-files '("/tmp/" "COMMIT_EDITMSG")))
-
-;;
-;; Input Method
-;; -----------------------------------------------------------------------------
-
-;; Daredevil SKK
-;; http://openlab.jp/skk/
-(use-package ddskk
-  :bind (("C-x C-j" . skk-mode))
-  :init (setq default-input-method "japanese-skk"))
+  :config (setq undohist-ignore-files '("/tmp/" "COMMIT_EDITMSG")))
 
 ;;
 ;; Project
 ;; -----------------------------------------------------------------------------
 
-;; Projectile
-;; https://projectile.readthedocs.io/en/latest/
 (use-package projectile
+  ;; https://projectile.readthedocs.io/en/latest/
+  :ensure t
+  :delight
   :init (projectile-mode)
   :config
   (setq projectile-globally-ignored-directories
         (append '("elpa" "node_modules" "vendor")
                 projectile-globally-ignored-directories)))
 
-;; counsel-projectile
-;; https://github.com/ericdanan/counsel-projectile
-;; (use-package counsel-projectile
-;;   :init (add-hook 'projectile-mode-hook 'counsel-projectile-mode))
-(use-package counsel-projectile
-  :init
-  (setq counsel-projectile-mode t))
-
-;;
-;; CVS
-;; -----------------------------------------------------------------------------
-
-;; Magit
-;; https://magit.vc
-(use-package magit
-  :bind (("C-x g" . magit-status)
-         ("C-x M-g" . magit-dispatch-popup)))
-
-;; Magithub
-;; https://github.com/vermiculus/magithub
-(use-package magithub
-  :after mgit
-  :config (magithub-feature-autoinject t))
-
-;; git-gutter-fringe+
-;; https://github.com/nonsequitur/git-gutter-fringe-plus
-(use-package git-gutter-fringe+
-  :if window-system
-  :init (global-git-gutter+-mode)
-  :config
-  (fringe-helper-define 'git-gutter-fr+-added '(top repeat) "xxxx....")
-  (fringe-helper-define 'git-gutter-fr+-deleted '(top repeat) "xxxx....")
-  (fringe-helper-define 'git-gutter-fr+-modified '(top repeat) "xxxx...."))
-
 ;;
 ;; Autocomplete
 ;; -----------------------------------------------------------------------------
 
-;; company
-;; https://company-mode.github.io
 (use-package company
+  ;; https://company-mode.github.io
+  :ensure t
+  :delight
   :init (global-company-mode))
-
-;; Company flow
-;; https://github.com/aaronjensen/company-flow
-(use-package company-flow
-  :init
-  (defun company-flow-setup ()
-    (add-to-list 'company-backends 'company-flow))
-  (add-hook 'company-mode-hook 'company-flow-setup))
-
-;; Company Go
-;; https://github.com/nsf/gocode/blob/master/emacs-company
-(use-package company-go
-  :init
-  (defun company-go-setup ()
-    (add-to-list 'company-backends 'company-go))
-  (add-hook 'company-mode-hook 'company-go-setup)
-  (setq gofmt-command (executable-find "goimports")))
 
 ;;
 ;; Syntax checker
 ;; -----------------------------------------------------------------------------
 
-;; Flycheck
-;; http://www.flycheck.org/en/latest/
 (use-package flycheck
-  :init (global-flycheck-mode)
-  :config
-  (setq flycheck-indication-mode 'right-fringe))
-
-;; flycheck-flow
-;; https://github.com/lbolla/emacs-flycheck-flow
-(use-package flycheck-flow
-  :init
-  (defun setup-flycheck-flow ()
-    (flycheck-add-next-checker 'javascript-eslint 'javascript-flow)
-    (flycheck-add-next-checker 'javascript-flow 'javascript-flow-coverage))
-  (add-hook 'flycheck-mode-hook 'setup-flycheck-flow))
+  ;; http://www.flycheck.org/en/lattest/
+  :ensure t
+  :init (global-flycheck-mode))
 
 ;;
-;; Translate
+;; Git
 ;; -----------------------------------------------------------------------------
 
-(use-package google-translate-default-ui
-  :bind (("C-c t" . google-translate-smooth-translate))
-  :config
-  (setq google-translate-translation-directions-alist
-        '(("en" . "ja") ("ja" . "en"))))
+(use-package magit
+  ;; https://magit.vc
+  :ensure t
+  :defer t
+  :bind (("C-x g" . magit-status)
+         ("C-x M-g" . magit-dispatch-popup)))
+
+(use-package gitconfig-mode
+  ;; https://github.com/magit/git-modes/blob/master/gitconfig-mode.el
+  :ensure t
+  :defer t)
+
+(use-package gitignore-mode
+  ;; https://github.com/magit/git-modes/blob/master/gitignore-mode.el
+  :ensure t
+  :defer t)
+
+(use-package gitattributes-mode
+  ;; https://github.com/magit/git-modes/blob/master/gitattributes-mode.el
+  :ensure t
+  :defer t)
 
 ;;
-;; Snippet
+;; Docker
 ;; -----------------------------------------------------------------------------
 
-;; Yet another snippet extension
-;; https://joaotavora.github.io/yasnippet/
-(use-package yasnippet
-  :init (yas-global-mode))
-
-;;
-;; Org
-;; -----------------------------------------------------------------------------
-
-(use-package org
+(use-package docker
+  ;; https://github.com/Silex/docker.el
+  :ensure t
+  :bind ("C-c d" . docker)
   :config
-  (setq org-agenda-files '("~/Agenda")))
+  (setq docker-image-run-arguments '("-i" "-t" "--rm")))
+
+(use-package dockerfile-mode
+  ;; https://github.com/spotify/dockerfile-mode
+  :ensure t
+  :defer t
+  :mode (("Dockerfile\\'" . dockerfile-mode)))
 
 ;;
 ;; Markdown
 ;; -----------------------------------------------------------------------------
 
-;; Markdown Mode
-;; https://jblevins.org/projects/markdown-mode/
 (use-package markdown-mode
-  :config
-  (setq markdown-command (executable-find "markdown-it")))
+  ;; https://jblevins.org/projects/markdown-mode
+  :ensure t
+  :defer t
+  :mode (("\\.md'" . markdown-mode)))
 
 ;;
-;; AsciiDoc
+;; Web
 ;; -----------------------------------------------------------------------------
 
-;; adoc-mode
-;; https://github.com/sensorflo/adoc-mode/wiki
-(use-package adoc-mode
-  :mode (("\\.adoc\\'" . adoc-mode)))
-
-;;
-;; HTML
-;; -----------------------------------------------------------------------------
-
-;; web-mode.el
-;; http://web-mode.org
 (use-package web-mode
+  ;; http://web-mode.org
+  :ensure t
+  :defer t
   :mode (("\\.phtml\\'" . web-mode)
          ("\\.tpl\\.php\\'" . web-mode)
          ("\\.[agj]sp\\'" . web-mode)
@@ -428,37 +355,80 @@
 ;; JavaScript
 ;; -----------------------------------------------------------------------------
 
-;; js2-mode
-;; https://github.com/mooz/js2-mode
 (use-package js2-mode
+  ;; https://github.com/mooz/js2-mode
+  :ensure t
+  :defer t
   :mode (("\\.jsx?\\'" . js2-jsx-mode))
   :config
   (setq js2-mode-show-strict-warnings nil
-        js2-mode-show-parse-errors nil))
+        js2-mode-show-parse-errors nil)
+  :init
+  (use-package add-node-modules-path
+    ;; https://github.com/codesuki/add-node-modules-path
+    :ensure t
+    :hook ((js2-mode . add-node-modules-path))))
+
+;;
+;; JSON
+;; -----------------------------------------------------------------------------
+
+(use-package json-mode
+  ;; https://github.com/joshwnj/json-mode
+  :ensure t
+  :defer t)
 
 ;;
 ;; TypeScript
 ;; -----------------------------------------------------------------------------
 
-;; Tide
-;; https://github.com/ananthakumaran/tide
-(use-package tide
+(use-package typescript-mode
+  ;; https://github.com/emacs-typescript/typescript.el
+  :ensure t
+  :defer t
   :init
-  (defun setup-tide-mode ()
-    (interactive)
-    (tide-setup)
-    (tide-hl-identifier-mode))
-  (add-hook 'typescript-mode-hook 'setup-tide-mode)
-  (add-hook 'before-save-hook 'tide-format-before-save))
+  (use-package add-node-modules-path
+    ;; https://github.com/codesuki/add-node-modules-path
+    :ensure t
+    :hook ((js2-mode . add-node-modules-path))))
+
+(use-package tide
+  ;; https://github.com/ananthakumaran/tide
+  :ensure t
+  :defer t
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)
+         (before-save . tide-format-before-save)))
+
+;; (use-package vue-mode
+;;   ;; https://github.com/AdamNiederer/vue-mode
+;;   :ensure t
+;;   :defer t)
 
 ;;
-;; Go
+;; YAML
 ;; -----------------------------------------------------------------------------
 
-;; go-mode
-;; https://github.com/dominikh/go-mode.el
-(use-package go-mode
-  :config
-  (add-hook 'before-save-hook 'gofmt-before-save))
+(use-package yaml-mode
+  ;; https://github.com/yoshiki/yaml-mode
+  :ensure t
+  :defer t)
+
+;; -----------------------------------------------------------------------------
+
+(setq backup-inhibited t
+      indent-tabs-mode nil
+      inhibit-startup-message t)
+
+(column-number-mode)
+
+(when window-system
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (add-to-list 'default-frame-alist '(width . 120))
+  (add-to-list 'default-frame-alist '(height . 40)))
+(unless window-system
+  (menu-bar-mode -1))
 
 ;;; init.el ends here
